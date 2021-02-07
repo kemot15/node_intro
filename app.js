0ddd5b1e3ec113dcbee3e3a6a83aaf2d7d9c9f67
Tomek <kemot15@tlen.pl>
2021-02-07 12:25:50 +0100

    init

---
+++ app.js
@@ -0,0 +1,4 @@
+const functions = require("./functions");
+
+functions.helloWorld();
+functions.add(1,2);
---
+++ functions.js
@@ -0,0 +1,7 @@
+const helloWorld = () => console.log("Hello World!");
+const add = (a,b) => console.log(a+b);
+
+module.exports = {
+    helloWorld,
+    add
+}
